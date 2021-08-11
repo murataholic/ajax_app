994193aeb3e98b5e0f3e8f6cb113aa8f5fb91623
murataholic <murataholic@gmail.com>
2021-08-11 21:55:36 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const submit = document.getElementById("submit"); // 投稿ボタンの要素を取得する記述
   submit.addEventListener("click", (e) => {         // 投稿ボタンをクリックしたときにイベント発火する記述
@@ -8,6 +22,16 @@ function post (){
     XHR.open("POST", "/posts", true);               // リクエストの内容を指定する記述
     XHR.responseType = "json";                      // レスポンスの形式を指定する記述
     XHR.send(formData);                             // フォームの内容をコントローラーへ送信するための記述
+    XHR.onload = () => {                            // レスポンスの受信に成功したときの処理を記述
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list"); // 新しいメモを挿入するための要素を取得する記述
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
